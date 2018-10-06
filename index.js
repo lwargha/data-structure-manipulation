fc0f6340e48574e29eb2e735b0108327292dc807
Joshua McKinney <mckinneyj@byui.edu>
2018-10-05 20:25:28 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var fs = require('fs'),
+dsv = require('d3-dsv'),
+stringify = require('json-stringify-pretty-compact'),
+csvString = fs.readFileSync('./countries.csv', 'utf8'),
+cities = dsv.csvParse(csvString, row =>{
+    return {
+        country: row.Country,
+        state: row.State,
+        name: row.City,
+        population: row.Population
+    };
+});
+
+// console.log(cities);
+
+function toUnique(ele,i,arr){
+    return arr.indexOf(ele) === i;
+}
+
+var countries = cities
+    .map(city => city.country)
+    .filter(toUnique)
+    .sort()
+    .map(function(countryName){
+        return {
+            name: countryName,
+            states : cities
+                .filter(city => city.country === countryName)
+                .map(city => city.state)
+                .filter(toUnique)
+                .sort()
+                .map(function(stateName){
+                    return {
+                        name:stateName,
+                        cities: cities
+                            .filter(city => city.state === stateName)
+                            .map(city => {
+                                return {
+                                name : city.name,
+                                population: city.population
+                                };
+                            })
+                            .sort((a,b)=> a.population - b.population)
+                    }
+                })
+        }
+    });
+
+console.log(stringify(countries));
